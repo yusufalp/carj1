6976e0af267a30d8dce1f156ff36d8e20c103d43
Yusuf Alp <alpyusuf007@gmail.com>
2020-12-03 07:23:25 -0800

    Calculating a bil for large groups

---
+++ BillCalculator/Calculator.java
@@ -0,0 +1,74 @@
+public class Calculator {
+  public static void main(String[] args) {
+
+    int numberOfPersons = 4;
+
+    double taxPercent = 8;
+    double tipPercent = 15;
+
+    double personOneAppetizerCost = 9.99;
+    double personOneMainCost = 23.99;
+    double personOneDessertCost = 10.29;
+    double personOneDrinkCost = 8.50;
+
+    double personTwoAppetizerCost = 12.99;
+    double personTwoMainCost = 18.99;
+    double personTwoDessertCost = 9.99;
+    double personTwoDrinkCost = 4.25;
+
+    double personThreeAppetizerCost = 10.49;
+    double personThreeMainCost = 26.79;
+    double personThreeDessertCost = 10.29;
+    double personThreeDrinkCost = 3.75;
+
+    double personFourAppetizerCost = 13.79;
+    double personFourMainCost = 25.99;
+    double personFourDessertCost = 4.49;
+    double personFourDrinkCost = 7.50;
+
+    String personOneName = "Adeline";
+    String personOneAppetizer = "Stuffed Ziti Fritta";
+    String personOneMain = "Shrimp Scampi";
+    String personOneDessert = "Sicilian Cheesecake";
+    String personOneDrink = "Raspberry Lemonade x 2";
+
+    String personTwoName = "Brixton";
+    String personTwoAppetizer = "Lasagna Fritta";
+    String personTwoMain = "Fettuccine Alfredo";
+    String personTwoDessert = "Lemon Cream cake";
+    String personTwoDrink = "Mango-Strawberry Iced Tea";
+
+    String personThreeName = "Cora";
+    String personThreeAppetizer = "Fried Mozzarella";
+    String personThreeMain = "Tour of Italy";
+    String personThreeDessert = "Zeppoli";
+    String personThreeDrink = "Fresh Brewed Iced Tea";
+
+    String personFourName = "Dean";
+    String personFourAppetizer = "Classic Shrimp Scampi Fritta";
+    String personFourMain = "Seafood Alfredo";
+    String personFourDessert = "Dolcini";
+    String personFourDrink = "Coke x 2";
+
+    boolean payTip = true;
+    boolean splitBillEvenly = true;
+  }
+}
+
+// Person Item Cost
+// Adeline Stuffed Ziti Fritta $9.99
+// Shrimp Scampi $23.99
+// Sicilian Cheesecake $10.29
+// Raspberry Lemonade x 2 $8.50
+// Brixton Lasagna Fritta $12.99
+// Fettuccine Alfredo $18.99
+// Lemon Cream Cake $9.99
+// Mango-Strawberry Iced Tea $4.25
+// Cora Fried Mozzarella $10.49
+// Tour of Italy $26.79
+// Zeppoli $10.29
+// Fresh Brewed Iced Tea $3.75
+// Dean Classic Shrimp Scampi Fritta $13.79
+// Seafood Alfredo $25.99
+// Dolcini $4.49
+// Coke x 2 $7.50
